6fe121288ca803b33843c56356fa707610511786
haydenych <heiheiyu127@gmail.com>
2025-09-20 19:36:06 -0400

    update scripts

--- scripts/extract_features_local.sh
+++ scripts/extract_features_local.sh
@@ -1,9 +1,11 @@
 python3 extract_features.py \
     --dataset_path ../data/train.csv \
     --output_path ../data/seq_embeddings/train \
-    --device cuda
+    --device mps
 
 python3 extract_features.py \
     --dataset_path ../data/test.csv \
     --output ../data/seq_embeddings/test \
-    --device cuda
+    --device mps
+
+    
